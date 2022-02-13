8960d9d72c85c9fa805aceb49cc8a33bc5969a7a
alpagu872 <83309872+alpagu872@users.noreply.github.com>
2022-02-13 23:55:57 +0300

    Music App End | Search bar added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -10,27 +10,41 @@ import {
 
 import music_data from './music-data.json'
 import SongCard from './components/SongCard'
+import SearchBar from './components/SearchBar'
 
 
 function App() {
+  const [list, setList] = useState(music_data)
 
   const renderSong = ({ item }) =>
-  <SongCard song= {item} />
+    <SongCard song={item} />
 
-  const renderSeperator = () => <View style={styles.seperator}/>
-  
+  const renderSeperator = () => <View style={styles.seperator} />
+
+  const handleSearch = text => {
+    const filteredList = music_data.filter(song => {
+      const searchedText = text.toLowerCase();
+
+      const currentTitle = song.title.toLowerCase();
+
+      return currentTitle.indexOf(searchedText) > -1;
+    })
+    setList(filteredList);
+  }
 
   return (
     <SafeAreaView style={styles.container}>
 
+      <SearchBar onSearch={handleSearch} />
+
       <View>
         <FlatList
-          data={music_data}
+          data={list}
           keyExtractor={item => item.id}
-          renderItem= {renderSong}
-          ItemSeparatorComponent = {renderSeperator}
+          renderItem={renderSong}
+          ItemSeparatorComponent={renderSeperator}
 
-  />
+        />
       </View>
 
     </SafeAreaView>
@@ -43,9 +57,9 @@ const styles = StyleSheet.create({
     flex: 1,
 
   },
-  seperator:{
-   borderWidth:1,
-   borderColor: '#e0e0e0',
+  seperator: {
+    borderWidth: 1,
+    borderColor: '#e0e0e0',
   }
 
 })
